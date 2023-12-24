62637493645316a4f6c05bc326f200f386e4099a
Mark Wetzel <mark@markwetzel.com>
2023-12-24 13:11:49 -0500

    Replace missing index file and add placeholder components/css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 // src/index.tsx
 import { createRoot } from 'react-dom/client'; 
-import App from './App'; 
+import App from './components/App'; 
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container); 
