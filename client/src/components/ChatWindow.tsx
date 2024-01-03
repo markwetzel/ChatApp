a42d6ca6ac080e469c28cf9953e7ccad1a860ef2
Mark Wetzel <mark@markwetzel.com>
2024-01-03 16:22:15 -0500

    Refactor timestamp code

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -1,28 +1,18 @@
 import { useContext } from "react";
 import { ChatContext } from "../context/ChatContext";
-
-function formatAMPM(date: Date) {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  const ampm = hours >= 12 ? "PM" : "AM";
-  const formattedHours = hours % 12 || 12; // Convert to 12-hour format
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-
-  return `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
-}
+import { DateTime } from "luxon";
 
 const ChatWindow = () => {
   const { messages } = useContext(ChatContext);
 
   return (
     <div>
-      {messages.map((message, index) => (
-        <p key={index}>{`[${formatAMPM(message.timestamp)}] ${
-          message.text
-        }`}</p>
-      ))}
+      {messages.map((message, index) => {
+        const localTime = DateTime.fromISO(
+          message.timestamp.toISOString()
+        ).toLocaleString(DateTime.TIME_WITH_SECONDS);
+        return <p key={index}>{`[${localTime}] ${message.text}`}</p>;
+      })}
     </div>
   );
 };
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
       directory: path.join(__dirname, "public"),
     },
     port: 3000,
-    open: true,
+    // open: true,
     hot: true,
   },
 };
